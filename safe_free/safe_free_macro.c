c63723fb6f515a18c6512cb3553dd09c3db19139
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2023-11-24 12:54:49 +0000

    Improved the function documentation for the custom _free() function

--- safe_free/safe_free_macro.c
+++ safe_free/safe_free_macro.c
@@ -6,13 +6,31 @@
 #define safe_free(ptr) _free((void **)&(ptr))
 
 /**
- * _free - frees dynamic memory memory in a much safer way
- * @ptr: a pointer to the memory location to free
+ * _free - frees dynamic memory allocated through a pointer to a pointer,
+ * providing a safer and more convenient approach.
+ *
+ * @ptr: a pointer to the pointer pointing to the memory location to be freed.
+ *
+ * Description: This function takes a pointer to a pointer pointing to a
+ * dynamically allocated memory location and frees the memory.
+ * It also sets the original pointer to NULL to prevent potential dangling
+ * pointers.
+ *
+ * Note: It is recommended to pass the address of the pointer to ensure proper
+ * behavior and avoid undefined behavior when attempting to free an already
+ * freed memory block.
+ *
+ * Example usage: `_free(&my_pointer);`
  */
 void _free(void **ptr)
 {
+	/*
+	 * check if the pointer and the memory it points to are not NULL
+	 * before attempting to free
+	 */
 	if (*ptr != NULL && ptr != NULL)
 	{
+		/* free the memory and set the original pointer to NULL */
 		free(*ptr);
 		*ptr = NULL;
 	}
